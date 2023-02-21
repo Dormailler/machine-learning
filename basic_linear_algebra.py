9b7e8f66c4a51e7812cfff1ea62391f2d0af4970
Dormailler <101726788+Dormailler@users.noreply.github.com>
2023-02-22 00:24:43 +0900

    Add files via upload

--- basic_linear_algebra.py
+++ basic_linear_algebra.py
@@ -1,53 +1,113 @@
 def vector_size_check(*vector_variables):
-    return None
+    return all(len(vector_variables[0]) == len(vector) for vector in vector_variables[1:])
 
 def vector_addition(*vector_variables):
-    return None
-
+    if vector_size_check(* vector_variables) == False:
+        raise ArithmeticError
+    return [sum(element) for element in zip(*vector_variables)]
 
 def vector_subtraction(*vector_variables):
     if vector_size_check(*vector_variables) == False:
         raise ArithmeticError
-    return None
+    return [element[0]*2 - sum(element) for element in zip(*vector_variables)]
 
+print(vector_subtraction([1, 3], [2, 4])) # Expected value: [-1, -1]
 
 def scalar_vector_product(alpha, vector_variable):
-    return None
+    return [alpha * element for element in vector_variable]
 
+print (scalar_vector_product(5,[1,2,3])) # Expected value: [5, 10, 15]
 
-def matrix_size_check(*matrix_variables):
-    return None
+def matrix_size_check(*matrix_variables):   
+    return all(len(matrix_variables[0]) == len(element) for element in matrix_variables[1:]
+    ) and all(len(matrix_variables[0][0]) == len(element) for element in matrix_variables[1:][0])
 
+matrix_x = [[2, 2], [2, 2], [2, 2]]
+matrix_y = [[2, 5], [2, 1]]
+matrix_z = [[2, 4], [5, 3]]
+matrix_w = [[2, 5], [1, 1], [2, 2]]
+
+print (matrix_size_check(matrix_x, matrix_y, matrix_z)) # Expected value: False
+print (matrix_size_check(matrix_y, matrix_z)) # Expected value: True
+print (matrix_size_check(matrix_x, matrix_w)) # Expected value: True
 
 def is_matrix_equal(*matrix_variables):
-    return None
+    from functools import reduce
+    return reduce(lambda a,b: a==b , zip(matrix_variables))
+matrix_x = [[2, 2], [2, 2]]
+matrix_y = [[2, 5], [2, 1]]
+matrix_z = [[2, 2], [2, 2]]
 
+
+print (is_matrix_equal(matrix_x, matrix_y, matrix_y, matrix_y)) # Expected value: False
+print (is_matrix_equal(matrix_x, matrix_z)) # Expected value: True
 
 def matrix_addition(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    return [[sum(row) for row in zip(*matrix)] for matrix in zip(*matrix_variables)]
+    
+matrix_x = [[2, 2], [2, 2]]
+matrix_y = [[2, 5], [2, 1]]
+matrix_z = [[2, 4], [5, 3]]
 
+print (matrix_addition(matrix_x, matrix_y)) # Expected value: [[4, 7], [4, 3]]
+print (matrix_addition(matrix_x, matrix_y, matrix_z)) # Expected value: [[6, 11], [9, 6]]
 
 def matrix_subtraction(*matrix_variables):
     if matrix_size_check(*matrix_variables) == False:
         raise ArithmeticError
-    return None
+    return [[row[0]*2 - sum(row) for row in zip(*matrix)] for matrix in zip(*matrix_variables)]
 
+matrix_x = [[2, 2], [2, 2]]
+matrix_y = [[2, 5], [2, 1]]
+matrix_z = [[2, 4], [5, 3]]
+
+print (matrix_subtraction(matrix_x, matrix_y)) # Expected value: [[0, -3], [0, 1]]
+print (matrix_subtraction(matrix_x, matrix_y, matrix_z)) # Expected value: [[-2, -7], [-5, -2]]
 
 def matrix_transpose(matrix_variable):
-    return None
+    return [[row for row in matrix] for matrix in zip(*matrix_variable)]
 
+matrix_w = [[2, 5], [1, 1], [2, 2]]
+
+print(matrix_transpose(matrix_w))
 
 def scalar_matrix_product(alpha, matrix_variable):
-    return None
+    return [[alpha * row for row in matrix ] for matrix in matrix_variable]
 
+matrix_x = [[2, 2], [2, 2], [2, 2]]
+matrix_y = [[2, 5], [2, 1]]
+matrix_z = [[2, 4], [5, 3]]
+matrix_w = [[2, 5], [1, 1], [2, 2]]
+
+print(scalar_matrix_product(3, matrix_x)) #Expected value: [[6, 6], [6, 6], [6, 6]]
+print(scalar_matrix_product(2, matrix_y)) #Expected value: [[4, 10], [4, 2]]
+print(scalar_matrix_product(4, matrix_z)) #Expected value: [[8, 16], [20, 12]]
+print(scalar_matrix_product(3, matrix_w)) #Expected value: [[6, 15], [3, 3], [6, 6]]
 
 def is_product_availability_matrix(matrix_a, matrix_b):
-    return None
+    return len(matrix_a[0]) == len(matrix_b)
 
+matrix_x= [[2, 5], [1, 1]]
+matrix_y = [[1, 1, 2], [2, 1, 1]]
+matrix_z = [[2, 4], [5, 3], [1, 3]]
+
+print(is_product_availability_matrix(matrix_y, matrix_z)) # Expected value: True
+print(is_product_availability_matrix(matrix_z, matrix_x)) # Expected value: True
+print(is_product_availability_matrix(matrix_z, matrix_w)) # Expected value: False //matrix_w가없습니다
+print(is_product_availability_matrix(matrix_x, matrix_z)) # Expected value: True
 
 def matrix_product(matrix_a, matrix_b):
     if is_product_availability_matrix(matrix_a, matrix_b) == False:
         raise ArithmeticError
-    return None
+    return [[sum(a*b for a,b in zip(row,col)) for col in zip(*matrix_b)] for row in matrix_a]
+
+matrix_x= [[2, 5], [1, 1]]
+matrix_y = [[1, 1, 2], [2, 1, 1]]
+matrix_z = [[2, 4], [5, 3], [1, 3]]
+
+print(matrix_product(matrix_y, matrix_z)) # Expected value: [[9, 13], [10, 14]]
+print(matrix_product(matrix_z, matrix_x)) # Expected value: [[8, 14], [13, 28], [5, 8]]
+print(matrix_product(matrix_x, matrix_x)) # Expected value: [[9, 15], [3, 6]]
+
